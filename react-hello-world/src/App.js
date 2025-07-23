6da93f435e9474277ec3df2d572af9b0f69646e9
alanIbanez <91506518+alanIbanez@users.noreply.github.com>
2025-07-23 01:24:53 +0000

    commit grupal

---
+++ react-hello-world/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import prueba from './prueba.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         CURRICULUM GRUPO
+        </p>
+        <img src={prueba} className="App-logo" alt="logo" />
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
